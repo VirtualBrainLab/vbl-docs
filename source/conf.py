e441714347e32d74fc82a4c3b346eb741e2f4ef2
Dan Birman <danbirman@gmail.com>
2023-09-12 16:22:58 -0700

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -53,8 +53,10 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
-# Include your custom CSS file
-html_css_files = ['custom.css']
+html_context = {
+    # ...
+    'css_files': ['custom.css'],
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
